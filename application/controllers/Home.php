ef25a753941f6abbf06f364650d982f28219591a
riyansasaja <riyanboyuhu@gmail.com>
2021-01-07 14:51:21 +0800

    rubah awal dashboard

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends CI_Controller
 
     public function index()
     {
+        $this->load->view('home/templates/headbar');
+        $this->load->view('home/templates/sidebar');
+        $this->load->view('home/templates/topbar');
         $this->load->view('home/v_dashboard');
+        $this->load->view('home/templates/footbar');
     }
 }
