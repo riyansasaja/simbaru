8da2633b3d95e3bd93a0b829b84bef7e2015c768
riyansasaja <riyanboyuhu@gmail.com>
2021-01-20 11:10:10 +0800

    Rubah Warna Main Sidebar dan Buat tabel pertama User

---
+++ application/views/admin/v_usermanagement.php
@@ -0,0 +1,49 @@
+<div class="container-fluid ">
+
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card">
+                    <div class="card-header">
+                        User Management
+                    </div>
+                    <div class="card-body">
+
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- end container -->
+    </div>
