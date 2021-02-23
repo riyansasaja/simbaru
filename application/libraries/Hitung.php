9b00c334460194c0a0cb8e300555c936971c1417
riyansasaja <riyanboyuhu@gmail.com>
2021-02-23 11:51:41 +0800

    rubah tulisan ModelHitung

--- application/libraries/Hitung.php
+++ application/libraries/Hitung.php
@@ -11,17 +11,17 @@ class Hitung
 
     public function countItem($data1, $data2, $data3)
     {
-        $this->ci->load->model('modeldata');
-        return $this->ci->modeldata->count1($data1, $data2, $data3)->num_rows();
+        $this->ci->load->model('ModelData');
+        return $this->ci->ModelData->count1($data1, $data2, $data3)->num_rows();
     }
     public function countall($table)
     {
-        $this->ci->load->model('modeldata');
-        return $this->ci->modeldata->countall($table)->num_rows();
+        $this->ci->load->model('ModelData');
+        return $this->ci->ModelData->countall($table)->num_rows();
     }
     public function countmore($data1, $data2, $data3)
     {
-        $this->ci->load->model('modeldata');
-        return $this->ci->modeldata->countmore($data1, $data2, $data3)->num_rows();
+        $this->ci->load->model('ModelData');
+        return $this->ci->ModelData->countmore($data1, $data2, $data3)->num_rows();
     }
 }
